6a9e92f30878c72134f55e0b099b788bb38c6238
lkogan14 <57683692+lkogan14@users.noreply.github.com>
2019-12-05 13:33:45 -0500

    updated with 4 separate redis clients

--- db/groups_server.py
+++ db/groups_server.py
@@ -4,24 +4,62 @@ import groups_pb2.py
 import groups_pb2_grpc.py
 
 
-redisClient=redis.StrictReids(host='localhost', port=6379, db=0) #open revis server
+redisClient_0=redis.StrictReids(host='localhost', port=6379, db=0) #open revis server 0
+redisClient_1=redis.StrictReids(host='localhost', port=6380, db=1) #open revis server 1
+redisClient_2=redis.StrictReids(host='localhost', port=6381, db=2) #open revis server 2
+redisClient_3=redis.StrictReids(host='localhost', port=6382, db=3) #open revis server 3
 
 class GroupManager(groups_pb2_grpc.Groups_ManagerServicer): #manager system
 	#all should return 1 if correct
-	def AddMember(self, request, context): 
-		return groups_pb2.AddMemberReply(redisClient.sadd(request.group_id, request.user_id))
+	def AddMember(self, request, context):
+		val=request.group_id%4
+		if (val==0):
+			return groups_pb2.AddMemberReply(redisClient_0.sadd(request.group_id, request.user_id))
+		elif (val==1):
+			return groups_pb2.AddMemberReply(redisClient_1.sadd(request.group_id, request.user_id))
+		elif (val==2):
+			return groups_pb2.AddMemberReply(redisClient_2.sadd(request.group_id, request.user_id))
+		return groups_pb2.AddMemberReply(redisClient_3.sadd(request.group_id, request.user_id))
 
 	def RemoveMember(self, request, context):
-		return groups_pb2.RemoveMemberReply(redisClient.srem(request.group_id, request.user_id))
+		val=request.group_id%4 
+		if (val==0):
+			return groups_pb2.AddMemberReply(redisClient_0.srem(request.group_id, request.user_id))
+		elif (val==1):
+			return groups_pb2.AddMemberReply(redisClient_1.srem(request.group_id, request.user_id))
+		elif (val==2):
+			return groups_pb2.AddMemberReply(redisClient_2.srem(request.group_id, request.user_id))
+		return groups_pb2.AddMemberReply(redisClient_3.srem(request.group_id, request.user_id))
 
 	def Contains(self, request, context):
-		return groups_pb2.ContainsReply(redisClient.sismember(request.group_id, request.user_id))
+		val=request.group_id%4 
+		if (val==0):
+			return groups_pb2.AddMemberReply(redisClient_0.sismember(request.group_id, request.user_id))
+		elif (val==1):
+			return groups_pb2.AddMemberReply(redisClient_1.sismember(request.group_id, request.user_id))
+		elif (val==2):
+			return groups_pb2.AddMemberReply(redisClient_2.sismember(request.group_id, request.user_id))
+		return groups_pb2.AddMemberReply(redisClient_3.sismember(request.group_id, request.user_id))
 
 	def AllMembers(self, request, context):
-		return groups_pb2.AllMembersReply(redisClient.smember(request.group_id))
+		val=request.group_id%4 
+		if (val==0):
+			return groups_pb2.AddMemberReply(redisClient_0.smember(request.group_id))
+		elif (val==1):
+			return groups_pb2.AddMemberReply(redisClient_1.smember(request.group_id))
+		elif (val==2):
+			return groups_pb2.AddMemberReply(redisClient_2.smember(request.group_id))
+		return groups_pb2.AddMemberReply(redisClient_3.smember(request.group_id))
 
 	def DeleteGroup(self, request, context):
-		return groups_pb2.DeleteGroupReply(redisClient.delete(request.group_id))
+		val=request.group_id%4 
+		if (val==0):
+			return groups_pb2.AddMemberReply(redisClient_0.delete(request.group_id))
+		elif (val==1):
+			return groups_pb2.AddMemberReply(redisClient_1.delete(request.group_id))
+		elif (val==2):
+			return groups_pb2.AddMemberReply(redisClient_2.delete(request.group_id))
+		return groups_pb2.AddMemberReply(redisClient_3.delete(request.group_id))
 
 
 
