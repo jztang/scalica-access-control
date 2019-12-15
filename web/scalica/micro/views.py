2102b9a4b7304b30fc039a7b9edd4023cddf1026
fjpardo <fjp265@nyu.edu>
2019-12-15 16:42:23 -0500

    deleting members from group

--- web/scalica/micro/views.py
+++ web/scalica/micro/views.py
@@ -182,15 +182,17 @@ def settings(request):
 @login_required
 def addGroup(request):
   #here i wanna call your method here
+  groupName = request.POST.get('newgroup')
+  print("group name top "+groupName)
   with grpc.insecure_channel('localhost:50052') as channel2:
     stub2 = groupDB_pb2_grpc.databaseStub(channel2)
-    stub2.addGroup(groupDB_pb2.addGroupRequest(userId = request.user.id, groupName = request.POST.get('newgroup')))
+    stub2.addGroup(groupDB_pb2.addGroupRequest(userId = request.user.id, groupName = groupName))
     print(stub2.getGroupNames(groupDB_pb2.getGroupNamesRequest(userId = request.user.id)))
 
   with grpc.insecure_channel('localhost:50052') as channel2:
     stub2 = groupDB_pb2_grpc.databaseStub(channel2)
-    print("group name from user "+request.POST.get('groupname'))
-    groupID = stub2.getGroupId(groupDB_pb2.getGroupRequest(groupName = request.POST.get('groupname'), userId = request.user.id))
+    print("group name from user "+groupName)
+    groupID = stub2.getGroupId(groupDB_pb2.getGroupRequest(groupName = groupName, userId = request.user.id))
     print(stub2.getGroupNames(groupDB_pb2.getGroupNamesRequest(userId = request.user.id)))
     print('group id is' + str(groupID.groupId))
 
