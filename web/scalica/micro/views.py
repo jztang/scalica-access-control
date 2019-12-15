8b09fe6cebd33fd940704861e3c8cfa2e53f3dfa
fjpardo <fjp265@nyu.edu>
2019-12-14 23:08:50 -0500

    testing dropdowns

--- web/scalica/micro/views.py
+++ web/scalica/micro/views.py
@@ -191,7 +191,7 @@ def getGroups(request):
   #here i wanna call your method here
   with grpc.insecure_channel('localhost:50052') as channel:
     stub = groupDB_pb2_grpc.databaseStub(channel)
-    groups = stub.getGroupNames(userId = request.user.id)
+    groups = stub.getGroupNames(groupDB_pb2.getGroupNamesRequest(userId = request.user.id))
     items = groups.split(",")
     print(groups)
     print(items)
