4fdaca5b5c8a97714e9ec8ffd9bde66d2ca08511
fjpardo <fjp265@nyu.edu>
2019-12-15 17:28:12 -0500

    deleting members from group

--- groupDatabase-Django/accessControl/groupDB_server.py
+++ groupDatabase-Django/accessControl/groupDB_server.py
@@ -62,7 +62,7 @@ class database(groupDB_pb2_grpc.databaseServicer):
 		except user.DoesNotExist:
 			return groupDB_pb2.deleteGroupReply(success = False)
 
-		print(filterSet)
+		print("filter set" + filterSet)
 		for i in filterSet:
 			if i.groupName == currentGroupName:
 				with grpc.insecure_channel('localhost:50051') as channel:
